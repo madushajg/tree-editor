19f07d5cab82eb19c8b7b769abd537ac967d3062
Madusha <madushajg@gmail.com>
2021-09-01 20:43:30 +0530

    Add initial expression definition

---
+++ src/models/definitions.ts
@@ -0,0 +1,40 @@
+interface Expression {
+    type: ("string" | "int" | "float" | "decimal" | "boolean")[],
+    kind: string,
+    expressionType: 
+      | Comparison
+      | TypeCheck
+      | Conditional
+      | Literal
+      | Arithmatic
+      | Expression
+  }
+  
+  interface Literal {
+    value: any
+  }
+  
+  interface Comparison {
+    lhs: Expression
+    operator: ">" | "<"
+    rhs: Expression
+  }
+  
+  interface Arithmatic {
+    lhs: Expression
+    operator: "+" | "-"
+    rhs: Expression
+  }
+  
+  interface TypeCheck {
+    lhs: Expression
+    operator: "is"
+    typeDescriptor: "string" | "int" | "float" | "boolean"
+  }
+  
+  interface Conditional {
+    condition: Expression
+    trueExpr: Expression
+    falseExpr: Expression
+  }
+  
